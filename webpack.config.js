16f696be186c534bb317bcad4076f130680d27d7
nicoflrs <floresni1996@gmail.com>
2022-01-25 12:24:41 -0800

    progress

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { env } = require('process')
 
 module.exports = {
-    mode: env.NODE_ENV,
+
     entry: path.join(__dirname, "src", "index.js"),
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Development',
-        template: 'index.html'
-      })],
-      output: {
+    output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              }    
+            }
+          },
+        ]
       },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Development',
+        template: path.join(__dirname, "src", "index.html")
+    })],
 }
