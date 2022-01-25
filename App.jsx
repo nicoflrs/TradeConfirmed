cb7c2de7bb9725bc25d3b31c23b28b87fb826fbc
nicoflrs <floresni1996@gmail.com>
2022-01-25 11:54:11 -0800

    solodolo

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-/*code goes here
 
-*/
+<h1>Hello World</h1>
+
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { env } = require('process')
+
+module.exports = {
+    mode: env.NODE_ENV,
+    entry: path.join(__dirname, "src", "index.js"),
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Development',
+        template: 'index.html'
+      })],
+      output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+      },
+}
