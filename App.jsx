e260d0fa5036375e2651550f70cc1a81be2b17f4
nicoflrs <floresni1996@gmail.com>
2022-01-26 13:29:11 -0800

    progress

--- App.jsx
+++ App.jsx
@@ -1,15 +1,17 @@
 import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-    }
         render () {
             return (
-                <div>
-                    <Submit />
-                    <Log />
+                <div>                   
+        <BrowserRouter>
+        <Routes>
+        <Route exact path="/" element={<Submit />} />
+        <Route exact path="/log" element={<Log />} />
+        </Routes>
+       </BrowserRouter>
                 </div>
             
             )
@@ -17,9 +19,6 @@ class App extends Component {
 }
 
 class Submit extends Component {
-    constructor (props) {
-        super(props);
-      }
       render () {
           return <div>
               <h2>Add a trade</h2>
@@ -60,16 +59,68 @@ class Submit extends Component {
             />
             <button type = "submit">Add</button>
         </form>
+        <Link to = 'log'>
+        <button>Fetch Log</button>
+        </Link>
           </div>
       }
 }
 
 class Log extends Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
+    
+        this.state = {
+          result: []
+        };
+      }
+    componentDidMount() {
+        fetch('http://localhost:3000/log')
+          .then(response => response.json())
+          .then((data) => { 
+            return this.setState({ result: data })});
       }
     render () {
-        return <a href = 'http://localhost:3000/log'><button>View Log</button></a>
+        if (!this.state.result) return (
+            <div>
+                <h1>Loading log, please wait...</h1>
+            </div>
+        )
+        const { result } = this.state;
+
+        if (!result) return null
+
+        if (!result.length) return (
+            <div>You have no trades logged.</div>
+        )
+
+        console.log(this.state.result[0])
+        const arr = [];
+        for (let i = 0; i < this.state.result.length; i++) {
+            arr.push([<td>{this.state.result[i].position}</td>, <td>{this.state.result[i].numcontracts}</td>, <td>{this.state.result[i].strategy}</td>, <td>{this.state.result[i].datebtosto}</td>, <td>{this.state.result[i].datebtcstc}</td>])
+        }
+        console.log(arr);
+        const resultArr = [];
+        for (let i = 0; i < arr.length; i++) {
+
+        }
+        // console.log(this.state.result[0][position])
+        return <div>
+            <table border = '1'>
+            <thead>
+            <tr>
+                <th>Position</th>
+                <th>Number of Contracts</th>
+                <th>Strategy</th>
+                <th>Date of BTO/STO</th>
+                <th>Date of BTC/STC</th>
+            </tr>
+            </thead>
+            <tbody>
+                {arr}
+            </tbody>
+            </table>
+        </div>
     }
 }
 
