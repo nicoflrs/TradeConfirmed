34d02df167fc60b8bb467a5e3cbb45f57bd1dad6
Nico <floresni1996@gmail.com>
2023-02-09 21:55:06 -0800

    spacing

--- client/components/ReturnButton.jsx
+++ client/components/ReturnButton.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ReturnButton = () => {
-	return (
-		<Link to='/homepage'>
-			<button id="homebutton">Return to Homepage</button>
-		</Link>
-	);
+  return (
+    <Link to='/homepage'>
+      <button id="homebutton">Return to Homepage</button>
+    </Link>
+  );
 };
 
 export default ReturnButton;
